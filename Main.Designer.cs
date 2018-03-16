c28ef711f4f4e9389eeb39518d71824708c246df
grass <ckshih@outlook.com>
2018-03-16 16:14:39 +0800

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace BT
+{
+    partial class mainWin
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWin));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.data = new System.Windows.Forms.RichTextBox();
+            this.time = new System.Windows.Forms.NumericUpDown();
+            this.comList = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btStatus = new System.Windows.Forms.Label();
+            this.btnTimer = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.timerTest = new System.Windows.Forms.Timer(this.components);
+            this.lblTime = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.fileName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCmd = new System.Windows.Forms.TextBox();
+            this.btn_cmd = new System.Windows.Forms.Button();
+            this.btnTypeOfOutput = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.time)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(12, 45);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(12, 82);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Enabled = false;
+            this.btnExport.Location = new System.Drawing.Point(273, 406);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(55, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Second";
+            // 
+            // data
+            // 
+            this.data.BackColor = System.Drawing.Color.White;
+            this.data.Location = new System.Drawing.Point(106, 100);
+            this.data.Name = "data";
+            this.data.ReadOnly = true;
+            this.data.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.data.Size = new System.Drawing.Size(451, 300);
+            this.data.TabIndex = 5;
+            this.data.Text = "";
+            // 
+            // time
+            // 
+            this.time.Location = new System.Drawing.Point(15, 193);
+            this.time.Maximum = new decimal(new int[] {
+            600,
+            0,
+            0,
+            0});
+            this.time.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(35, 22);
+            this.time.TabIndex = 6;
+            this.time.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // comList
+            // 
+            this.comList.FormattingEnabled = true;
+            this.comList.Location = new System.Drawing.Point(80, 16);
+            this.comList.Name = "comList";
+            this.comList.Size = new System.Drawing.Size(113, 20);
+            this.comList.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "COM Name";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(199, 14);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 9;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btStatus
+            // 
+            this.btStatus.AutoSize = true;
+            this.btStatus.ForeColor = System.Drawing.Color.Red;
+            this.btStatus.Location = new System.Drawing.Point(292, 19);
+            this.btStatus.Name = "btStatus";
+            this.btStatus.Size = new System.Drawing.Size(149, 12);
+            this.btStatus.TabIndex = 10;
+            this.btStatus.Text = "尚未連接至任何藍芽裝置。";
+            // 
+            // btnTimer
+            // 
+            this.btnTimer.Location = new System.Drawing.Point(12, 160);
+            this.btnTimer.Name = "btnTimer";
+            this.btnTimer.Size = new System.Drawing.Size(75, 23);
+            this.btnTimer.TabIndex = 11;
+            this.btnTimer.Text = "定時";
+            this.btnTimer.UseVisualStyleBackColor = true;
+            this.btnTimer.Click += new System.EventHandler(this.btnTimer_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Location = new System.Drawing.Point(12, 121);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(75, 23);
+            this.btnClean.TabIndex = 12;
+            this.btnClean.Text = "Clean";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // timerTest
+            // 
+            this.timerTest.Tick += new System.EventHandler(this.timerTest_Tick);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(40, 249);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(11, 12);
+            this.lblTime.TabIndex = 13;
+            this.lblTime.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 12);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Second";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Remain :";
+            // 
+            // fileName
+            // 
+            this.fileName.Location = new System.Drawing.Point(168, 406);
+            this.fileName.Name = "fileName";
+            this.fileName.Size = new System.Drawing.Size(99, 22);
+            this.fileName.TabIndex = 16;
+            this.fileName.Text = "data";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 411);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 12);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "File Name :";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(15, 279);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 18;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(168, 45);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(303, 22);
+            this.txtSend.TabIndex = 19;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(477, 45);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 20;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(104, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 12);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Send Data";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(104, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 12);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Command";
+            // 
+            // txtCmd
+            // 
+            this.txtCmd.Location = new System.Drawing.Point(168, 73);
+            this.txtCmd.Name = "txtCmd";
+            this.txtCmd.Size = new System.Drawing.Size(303, 22);
+            this.txtCmd.TabIndex = 23;
+            // 
+            // btn_cmd
+            // 
+            this.btn_cmd.Enabled = false;
+            this.btn_cmd.Location = new System.Drawing.Point(477, 71);
+            this.btn_cmd.Name = "btn_cmd";
+            this.btn_cmd.Size = new System.Drawing.Size(75, 23);
+            this.btn_cmd.TabIndex = 24;
+            this.btn_cmd.Text = "Send";
+            this.btn_cmd.UseVisualStyleBackColor = true;
+            this.btn_cmd.Click += new System.EventHandler(this.btn_cmd_Click);
+            // 
+            // btnTypeOfOutput
+            // 
+            this.btnTypeOfOutput.Location = new System.Drawing.Point(477, 404);
+            this.btnTypeOfOutput.Name = "btnTypeOfOutput";
+            this.btnTypeOfOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnTypeOfOutput.TabIndex = 25;
+            this.btnTypeOfOutput.Text = "String";
+            this.btnTypeOfOutput.UseVisualStyleBackColor = true;
+            this.btnTypeOfOutput.Click += new System.EventHandler(this.btnTypeOfOutput_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(439, 409);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 12);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Type:";
+            // 
+            // mainWin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 456);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnTypeOfOutput);
+            this.Controls.Add(this.btn_cmd);
+            this.Controls.Add(this.txtCmd);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.fileName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btnTimer);
+            this.Controls.Add(this.btStatus);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comList);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainWin";
+            this.Text = "Bluetooth Receiver";
+            ((System.ComponentModel.ISupportInitialize)(this.time)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox data;
+        private System.Windows.Forms.NumericUpDown time;
+        private System.Windows.Forms.ComboBox comList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label btStatus;
+        private System.Windows.Forms.Button btnTimer;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Timer timerTest;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox fileName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCmd;
+        private System.Windows.Forms.Button btn_cmd;
+        private System.Windows.Forms.Button btnTypeOfOutput;
+        private System.Windows.Forms.Label label8;
+
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace BT
+{
+    public partial class mainWin : Form
+    {
+        private SerialPort inPort;
+        private bool receiveStatus = false;
+        private Thread refreshData;
+        TimeSpan RemainTime;
+        TimeSpan SubTime = new TimeSpan(0, 0, 0, 0, 100);
+        bool isStr = true;
+
+        public mainWin()
+        {
+            InitializeComponent();
+            SerialPortSearch();
+            
+        }
+
+        private void SerialPortSearch()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            for (int i = 0; i < ports.Length; i++)
+                comList.Items.Add(ports[i]);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            try
+            {
+                String COM = comList.SelectedItem.ToString();
+                inPort = new SerialPort(COM, 9600);
+                inPort.Open();
+                if (inPort.IsOpen == true)
+                {
+                    btStatus.Text = "已連接成功!";
+                    btStatus.ForeColor = Color.Blue;
+                    btnStart.Enabled = true;
+                    btnStop.Enabled = true;
+                    btnExport.Enabled = true;
+                    btnSend.Enabled = true;
+                    btn_cmd.Enabled = true;
+                    refreshData = new Thread(dataRefrsh);
+                    refreshData.Start();
+                }
+            }
+            catch (System.IO.IOException)
+            {
+                btStatus.Text = "連接失敗! 請重新選擇其他COM使用。";
+                btStatus.ForeColor = Color.Red;
+                btnConnect.Enabled = true;
+            }
+            catch (System.NullReferenceException)
+            {
+                btStatus.Text = "選項不得為空，請重新選擇其他COM使用。";
+                btStatus.ForeColor = Color.Red;
+                btnConnect.Enabled = true;
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                btStatus.Text = "存取被拒絕! 請重新選擇其他COM使用。";
+                btStatus.ForeColor = Color.Red;
+                btnConnect.Enabled = true;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //inPort.Write("1");
+            receiveStatus = true;
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            
+           
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+           // inPort.Write("0");
+            receiveStatus = false;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+
+        }
+
+        private void btnTimer_Click(object sender, EventArgs e)
+        {
+            int t = (int)time.Value;
+            RemainTime = new TimeSpan(0, 0, t);
+            timerTest.Start();
+            try
+            {
+                //inPort.Write("1");
+                receiveStatus = true;
+                btnStart.Enabled = false;
+                btnStop.Enabled = false;
+                btnTimer.Enabled = false;
+            }
+            catch (Exception) { }
+            
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            /*try
+            {*/
+                String path = fileName.Text +".txt";
+                FileInfo file = new FileInfo(path);
+                StreamWriter sw = file.CreateText();
+                sw.WriteLine(data.Text);
+                sw.Flush();
+                sw.Close();
+           /* }
+            catch (Exception){}*/
+
+        }
+        public static string ToHexString(byte[] bytes) // 0xae00cf => "AE00CF "
+        {
+            string hexString = string.Empty;
+            if (bytes != null)
+            {
+                StringBuilder strB = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    strB.Append(bytes[i].ToString("X2"));
+                }
+                hexString = strB.ToString();
+            }
+            return hexString;
+        }
+
+        delegate void SetNumCallback(int newText);
+        private void receiveNumData(int str)
+        {
+            if (this.InvokeRequired)
+            {
+                SetNumCallback update = new SetNumCallback(receiveNumData);
+                this.Invoke(update, new object[] { str });
+            }
+            else
+            {
+                data.Text += str +" ";
+                data.SelectionStart = data.Text.Length;
+                data.ScrollToCaret();
+            }
+        }
+
+
+        delegate void SetTextCallback(string newText);
+        private void receiveData(String str)
+        {
+            if (this.InvokeRequired)
+            {
+                SetTextCallback update = new SetTextCallback(receiveData);
+                this.Invoke(update, new object[] { str });
+            }
+            else
+            {
+                data.Text += str + "\n";
+                data.SelectionStart = data.Text.Length;
+                data.ScrollToCaret();
+            }
+        }
+
+        private void dataRefrsh()
+        {
+            while (true)
+            {
+                if (receiveStatus == true)
+                {
+                    try
+                    {
+                        if (isStr)
+                        {
+                            string str = inPort.ReadLine();
+                            receiveData(str);
+                        }
+                        else
+                        {
+                            int tmp = inPort.ReadByte();
+                            if ( tmp == 0x80)
+                            {
+                                Byte[] buffer = new Byte[18];
+                                buffer[0] = 0x80;
+                                int len = inPort.Read(buffer, 1, buffer.Length-1);
+                                String str = ToHexString(buffer);
+                                if (buffer[1] == 0x00 && buffer[2] == 0x00 && buffer[3] == 0x7f && buffer[4]==0x00)
+                                    receiveData(str);
+                            }
+                        }
+                    }
+                    catch (TimeoutException) { }
+                }
+                else
+                {
+                    try
+                    {
+                        string str = inPort.ReadLine();
+                    }
+                    catch (TimeoutException) { }
+                }
+            }
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            data.Text = "";
+        }
+
+        private void timerTest_Tick(object sender, EventArgs e)
+        {
+            RemainTime = RemainTime.Subtract(SubTime);
+            lblTime.Text = "" + RemainTime.Seconds;
+            if (RemainTime.TotalMilliseconds == 0)
+            {
+                inPort.Write("0");
+                receiveStatus = false;
+                timerTest.Stop();
+                btnTimer.Enabled = true;
+                btnStart.Enabled = true;
+                btnStop.Enabled = true;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (refreshData != null)
+                refreshData.Abort();
+            if (inPort != null)
+                inPort.Close();
+            Application.Exit();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            inPort.Write(txtSend.Text);
+            txtSend.Text = "";
+        }
+
+        private void btn_cmd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String cmd_str = txtCmd.Text;
+                byte[] Command = Encoding.ASCII.GetBytes(cmd_str);
+                byte[] cmd = new byte[Command.Length + 2];
+                for (int i = 0; i < Command.Length; i++)
+                    cmd[i] = Command[i];
+                cmd[Command.Length] = 0x0d;
+                cmd[Command.Length + 1] = 0x0a;
+
+                inPort.Write(cmd, 0, cmd.Length);
+                txtCmd.Text = "";
+            }
+            catch (Exception) { }
+        }
+
+        private void btnTypeOfOutput_Click(object sender, EventArgs e)
+        {
+            isStr = (isStr == true) ? false : true;
+            btnTypeOfOutput.Text = (isStr == true) ? "String" : "Hex";
+        }
+    }
+}
